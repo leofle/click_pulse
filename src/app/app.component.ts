5f2bccec80920a25def49ef4516535a42db7f614
leofle <leofle@gmail.com>
2016-08-23 14:58:55 +0300

    adding change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
   <div class="app_wrapper">
     <h1>{{title}}</h1>
@@ -30,7 +31,17 @@ import { FooterComponent } from './footer/footer.component';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
   title = 'clicksoftware';
   sub_title = 'ClickPulse';
   today: number = Date.now();
+
+  constructor(private ref: ChangeDetectorRef) {
+      // ref.detach();
+      setInterval(() => {
+        this.today = Date.now();
+        this.ref.markForCheck();
+        // console.log(this.today);
+      }, 1000);
+    }
 }
