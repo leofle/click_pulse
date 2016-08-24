1503379ae271838e69dd72b02a40570519a1f16e
leofle <leofle@gmail.com>
2016-08-24 14:29:03 +0300

    adding availability

--- src/app/availability/availability.component.ts
+++ src/app/availability/availability.component.ts
@@ -4,9 +4,40 @@ import { Component } from '@angular/core';
   template: `
   <div class="availability_wrapper">
     <div class="inner_availability">
-      <p>Here goes availability</p>
+      <div class="calendar_container" *ngFor="let pod of podim; let i = index;">
+        <div class="pod_name">{{ pod.pod_name }}</div>
+        <div class="pod_calendar">
+          <div class="daily_capacity {{pod.capacity.dates[0].today}}"></div>
+        </div>
+      </div>
     </div>
   </div>`,
   styleUrls: ['availability.component.scss']
 })
-export class AvailabilityComponent { }
+export class AvailabilityComponent {
+  podim:Array<any>;
+constructor(){
+  this.podim = [
+    {
+    "pod_name":"Schedule",
+    "capacity":{"dates":[
+      {"today":"done"}
+    ]}
+  },
+  {
+  "pod_name":"Mobile",
+    "capacity":{"dates":[
+      {"today":"done"}
+    ]}
+  },
+  {
+  "pod_name":"Integration",
+    "capacity":{"dates":[
+      {"today":"done"}
+    ]}
+  }
+]
+
+}
+
+}
