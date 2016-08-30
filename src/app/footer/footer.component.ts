63edccd3bc951d89d9d2cf83395138af8f057f14
leofle <leofle@gmail.com>
2016-08-30 10:54:26 +0300

    footer layout fix for mobile

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,7 +4,17 @@ import { Component, OnInit } from '@angular/core';
   selector: 'pulse-footer',
   template: `
   <div class="footer_wrapper">
-    <p>clicksoftware 2016</p>
+    <ul class="inner_footer">
+      <li>
+        <a href="#">Privacy statement</a>
+      </li>
+      <li>
+        <a href="www.clicksoftware.com">clicksoftware.com</a>
+      </li>
+      <li>
+        <a href="#">clicksoftware 2016</a>
+      </li>
+    </ul>
   </div>
   `,
   styleUrls: ['footer.component.scss']
