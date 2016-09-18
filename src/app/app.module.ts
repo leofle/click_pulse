4c7ae31e901e00e05842c28d36a369256fba77bd
leofle <liofleishman@MBP.local>
2016-09-18 22:17:59 +0300

    karma testing app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
+import { NgModule, ApplicationRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { routing, appRoutingProviders } from './app.routing';
+import { HeaderComponent } from './header/header.component';
 import { AvailabilityComponent } from './availability/availability.component';
 import { StatusComponent } from './status/status.component';
 import { HttpModule }     from '@angular/http';
 import { MdButtonModule } from '@angular2-material/button';
 import { MdCardModule } from '@angular2-material/card';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { DateComponent } from './date/date.component';
 import { CardStatusComponent } from './card-status/card-status.component';
 import { ErrorCardComponent } from './error-card/error-card.component';
@@ -37,6 +37,7 @@ import { RegionCardComponent } from './region-card/region-card.component';
     MdButtonModule,
     MdCardModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [appRoutingProviders],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
